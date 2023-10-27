a08c0127d38a2bf13af2dd3a38b038fd9c47863a
Jan <janschneider@gmx.at>
2023-10-27 10:19:14 +0200

    Tic Tac Toe V1 (kommentiert)

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -9,6 +9,7 @@ public class TicTacToe {
     static ArrayList<Integer> playerPositions = new ArrayList<>();
     static ArrayList<Integer> cpuPositions = new ArrayList<>();
 
+    // Initialisierung Spielbrett
     public static void main(String[] args) {
         char[][] gameBoard = {
                 {' ', '|', ' ', '|', ' '},
@@ -18,16 +19,20 @@ public class TicTacToe {
                 {' ', '|', ' ', '|', ' '}
         };
 
+        // Anfangszustand ausgeben
         printGameBoard(gameBoard);
 
+        // Eingabe- und Zufallsobjekte
         Scanner scan = new Scanner(System.in);  // Einmalige Initialisierung
         Random rand = new Random();
 
+        // Game Loop
         while (true) {
 
-            // Spielerzug
+            // Spieleingabe
             System.out.println("Um einen Zug zu machen, bitte eine Nummer (1 - 9) eintragen und mit Enter bestätigen:");
             int playerPos = -1;
+
             while (true) {
                 try {
                     playerPos = scan.nextInt();
@@ -40,13 +45,16 @@ public class TicTacToe {
                 System.out.println("Ungültige Eingabe, bitte erneut versuchen:");
             }
 
+            // Spieler-Spielzug platzieren
             placePiece(gameBoard, playerPos, "player");
+
+            // Spielende prüfen
             if (isGameFinished()) {
                 printGameBoard(gameBoard);
                 break;
             }
 
-            // Computerzug
+            // Computer-Spielzug berechnen
             int cpuPos;
             while (true) {
                 cpuPos = rand.nextInt(9) + 1;
@@ -55,9 +63,11 @@ public class TicTacToe {
                 }
             }
 
+            // Computer-Spielzug platzieren und ausgeben
             placePiece(gameBoard, cpuPos, "cpu");
             printGameBoard(gameBoard);
 
+            // Spielende prüfen
             if (isGameFinished()) {
                 break;
             }
@@ -65,6 +75,7 @@ public class TicTacToe {
         scan.close();
     }
 
+    // Spielbrett ausgeben
     public static void printGameBoard(char[][] gameBoard) {
         for (char[] row : gameBoard) {
             for (char c : row) {
@@ -74,6 +85,7 @@ public class TicTacToe {
         }
     }
 
+    // Spielzug auf dem Spielbrett platzieren
     public static void placePiece(char[][] gameBoard, int pos, String user) {
         char symbol = (user.equals("player")) ? 'X' : 'O';
         switch (pos) {
@@ -114,6 +126,7 @@ public class TicTacToe {
         }
     }
 
+    // Überprüfen, ob das Spiel beendet ist
     public static boolean isGameFinished() {
         if (checkWinner().length() > 0) {
             System.out.println(checkWinner());
@@ -122,6 +135,7 @@ public class TicTacToe {
         return false;
     }
 
+    // Sieger oder Unentschieden ermitteln
     public static String checkWinner() {
         List<List<Integer>> winningConditions = Arrays.asList(
                 Arrays.asList(1, 2, 3),
@@ -134,6 +148,7 @@ public class TicTacToe {
                 Arrays.asList(7, 5, 3)
         );
 
+        // Sieger ermitteln
         for (List<Integer> condition : winningConditions) {
             if (playerPositions.containsAll(condition)) {
                 return "Herzlichen Glückwunsch, du hast gewonnen!";
@@ -142,9 +157,12 @@ public class TicTacToe {
             }
         }
 
+        // Unentschieden
         if (playerPositions.size() + cpuPositions.size() == 9) {
             return "Unentschieden!";
         }
+
+        // Kein Sieger
         return "";
     }
 }
